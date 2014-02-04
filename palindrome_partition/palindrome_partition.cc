d6ef7e7eb18d3fffdc6ee1adc6c7bf68743b25b4
bwzhou <bwzhou@pyxis.ecn.purdue.edu>
2014-02-03 15:59:42 -1000

    palindrome partition

--- palindrome_partition/palindrome_partition.cc
+++ palindrome_partition/palindrome_partition.cc
@@ -1,17 +1,56 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
 int minCut(string s) {
     const int n = s.size();
-    int cost[n][n];
+    bool **pal = new bool*[n];
+    for (int i = 0; i < n; ++i) {
+        pal[i] = new bool[n];
+        for (int j = 0; j < n; ++j) {
+            pal[i][j] = false;
+        }
+    }
     for (int len = 1; len <= n; ++len) {
-        for (int i = 0; i <= n - len; ++i) {
-            int j = i + len - 1;
-            cost[i][j] = len - 1;
-            for (int k = i; k < j; ++k) {
-                cost[i][j] = min(cost[i][j], cost[i][k] + cost[k + 1][j]);
-            }
-            if ((i + 1 > j - 1 || cost[i + 1][j - 1] == 0) && s[i] == s[j]) {
-                cost[i][j] = 0;
+        for (int start = 0; start <= n - len; ++start) {
+            int end = start + len - 1;
+            if (start == end ||
+                ((start == end - 1 || pal[start + 1][end - 1]) &&
+                 s[start] == s[end])) {
+                pal[start][end] = true;
             }
         }
     }
-    return cost[0][n - 1];
+
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        cout << "pal[" << i << "][" << j << "]=" << pal[i][j] << endl;
+      }
+    }
+    
+    int *cost = new int[n];
+    cost[0] = 0;
+    for (int i = 1; i < n; ++i) {
+        cost[i] = i;
+        if (pal[0][i]) {
+          cost[i] = 0;
+          continue;
+        }
+        for (int j = 0; j < i; ++j) {
+            if (pal[j + 1][i]) {
+                cost[i] = min(cost[i], cost[j] + 1);
+            }
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+      cout << "cost[" << i << "]=" << cost[i] << endl;
+    }
+
+    return cost[n - 1];
+}
+
+int main(int argc, char **argv) {
+  cout << minCut(argv[1]) << endl;
 }
