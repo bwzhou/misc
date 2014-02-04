013e2f0c449852c319081b9e23c3ab276909fe2d
bwzhou <bwzhou@pyxis.ecn.purdue.edu>
2014-02-03 19:16:25 -1000

    all partitions of palindromes

--- palindrome_partition/palindrome_partition.cc
+++ palindrome_partition/palindrome_partition.cc
@@ -1,5 +1,8 @@
 #include <string>
 #include <iostream>
+#include <iterator>
+#include <map>
+#include <vector>
 
 using namespace std;
 
@@ -51,6 +54,58 @@ int minCut(string s) {
     return cost[n - 1];
 }
 
+void partitionHelper(string &s, int start, map<pair<int, int>, bool> &pal,
+                     vector<string> &buf, vector<vector<string>> &result) {
+    int n = s.size();
+    if (start == n) {
+        result.emplace_back(buf);
+        return;
+    }
+    for (int end = start; end < n; ++end) {
+        if (pal.find(make_pair(start, end)) == pal.end()) {
+            if (start == end ||
+                ((start + 1 == end ||
+                  (pal.count(make_pair(start, end)) &&
+                   pal[make_pair(start, end)])) &&
+                 s[start] == s[end])) {
+                pal[make_pair(start, end)] = true;
+            } else {
+                bool isPalindrome = true;
+                for (int l = start, h = end; l < h; ++l, --h) {
+                    if (s[l] != s[h]) {
+                        isPalindrome = false;
+                        break;
+                    }
+                }
+                pal[make_pair(start, end)] = isPalindrome;
+
+                cout << "pal[" << start << "][" << end << "]="
+                     << pal[make_pair(start, end)] << endl;
+            }
+        }
+        if (pal[make_pair(start, end)]) {
+            buf.emplace_back(s.substr(start, end - start + 1));
+            partitionHelper(s, end + 1, pal, buf, result);
+            buf.pop_back();
+        }
+    }
+}
+
+vector<vector<string>> partition(string s) {
+    map<pair<int, int>, bool> pal;
+    vector<vector<string>> result;
+    vector<string> buf;
+    partitionHelper(s, 0, pal, buf, result);
+    for (auto &each : pal) {
+      cout << "pal: " << each.first.first << " " << each.first.second << " " << each.second << endl;
+    }
+    return result;
+}
+
 int main(int argc, char **argv) {
   cout << minCut(argv[1]) << endl;
+  for (auto &each : partition(argv[1])) {
+    copy(each.begin(), each.end(), ostream_iterator<string>(cout, " "));
+    cout << endl;
+  }
 }
