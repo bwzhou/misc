bab04443aa79b819745b927d72331f4f05fd736b
Bowen Zhou <bwzhou@hotmail.com>
2014-03-03 10:31:38 -1000

    update

--- zigzag/zigzag.cc
+++ zigzag/zigzag.cc
@@ -8,7 +8,7 @@ string convert(string s, int nRows) {
       return s;
     }
     for (int i = 0; i < nRows; ++i) {
-        for (int j = 0; j < s.size() - i; j += (nRows - 1) * 2) {
+        for (int j = 0; j < (int) s.size() - i; j += (nRows - 1) * 2) {
             res += s[i + j];
             if (i != 0 && i != nRows - 1 && i + j + (nRows - 1 - i) * 2 < s.size()) {
                 res += s[i + j + (nRows - 1 - i) * 2];
