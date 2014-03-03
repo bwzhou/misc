d063892f0375059a8ca8b1abd4f2571263fbfbfd
Bowen Zhou <bwzhou@hotmail.com>
2014-03-03 10:30:42 -1000

    update

---
+++ zigzag/zigzag.cc
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+using namespace std;
+string convert(string s, int nRows) {
+    string res;
+    if (nRows == 1) {
+      return s;
+    }
+    for (int i = 0; i < nRows; ++i) {
+        for (int j = 0; j < s.size() - i; j += (nRows - 1) * 2) {
+            res += s[i + j];
+            if (i != 0 && i != nRows - 1 && i + j + (nRows - 1 - i) * 2 < s.size()) {
+                res += s[i + j + (nRows - 1 - i) * 2];
+            }
+        }
+    }
+    return res;
+}
+
+int main(int argc, char **argv) {
+    cout << convert(argv[1], atoi(argv[2])) << endl;
+}
