d605d06e7102760297e3ea0afa655b28aaa62ad4
bwzhou <bwzhou@hotmail.com>
2014-03-16 16:19:29 -0400

    Update 147.cpp

--- leetcode/147.cpp
+++ leetcode/147.cpp
@@ -8,6 +8,7 @@
  */
 class Solution {
 public:
+/*
     ListNode *insertionSortList(ListNode *head) {
         if (head == NULL) {
             return NULL;
@@ -38,5 +39,24 @@ public:
             p = tail->next;
         }
         return head;
+    }
+*/
+    ListNode *insertionSortList(ListNode *head) {
+        ListNode dummy(0);
+        dummy.next = NULL;
+        ListNode *curr = head;
+        while (curr) {
+            ListNode *prev = &dummy;
+            ListNode *next = dummy.next;
+            while (next && next->val < curr->val) {
+                prev = next;
+                next = next->next;
+            }
+            prev->next = curr;
+            ListNode *tmp = curr->next;
+            curr->next = next;
+            curr = tmp;
+        }
+        return dummy.next;
     }
 };
