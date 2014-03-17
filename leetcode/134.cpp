fb7d176a772d413886b347cf44d67970557c2491
bwzhou <bwzhou@hotmail.com>
2014-03-17 16:30:16 -0400

    Update 134.cpp

--- leetcode/134.cpp
+++ leetcode/134.cpp
@@ -5,12 +5,12 @@ public:
         int sumfromstart = 0;
         int start = 0;
         for (int i = 0; i < gas.size(); ++i) {
-            sum += gas[i] - cost[i];
-            sumfromstart += gas[i] - cost[i];
             if (sumfromstart < 0) {
                 sumfromstart = 0;
-                start = i + 1;
+                start = i;
             }
+            sum += gas[i] - cost[i];
+            sumfromstart += gas[i] - cost[i];
         }
         if (sum >= 0) {
             return start;
