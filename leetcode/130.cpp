e68b1359cbe2900e07d9cee5e71f6049acf0fc63
bwzhou <bwzhou@hotmail.com>
2014-03-19 21:26:15 -0400

    Iterative Deepening DFS
    Plain DFS causes runtime error due to stack overflow.

--- leetcode/130.cpp
+++ leetcode/130.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*
     bool check(int i, int j, vector<vector<char>> &board, vector<vector<bool>> &liveness) {
         int x = board.size();
         int y = board[0].size();
@@ -61,6 +62,63 @@ public:
             for (int j = 0; j < y; ++j) {
                 if (board[i][j] == 'O' && !liveness[i][j]) {
                     board[i][j] = 'X';
+                }
+            }
+        }
+    }
+*/
+    void mark(vector<vector<char>> &board, int x, int y) {
+        vector<pair<int, int>> worklist;
+        worklist.push_back(make_pair(x, y));
+        while (!worklist.empty()) {
+            x = worklist.back().first;
+            y = worklist.back().second;
+            worklist.pop_back();
+            iterativeDeepening(board, x, y, 0, worklist);
+        }
+    }
+    void iterativeDeepening(vector<vector<char>> &board, int x, int y, 
+                            int depth, vector<pair<int, int>> &worklist) {
+        if (x >= 0 && x < board.size() && y >= 0 && y < board[0].size() && board[x][y] == 'O') {
+            board[x][y] = 'Q';
+            if (depth < 10) {
+                iterativeDeepening(board, x + 1, y, depth + 1, worklist);
+                iterativeDeepening(board, x, y + 1, depth + 1, worklist);
+                iterativeDeepening(board, x - 1, y, depth + 1, worklist);
+                iterativeDeepening(board, x, y - 1, depth + 1, worklist);
+            } else {
+                worklist.push_back(make_pair(x + 1, y));
+                worklist.push_back(make_pair(x, y + 1));
+                worklist.push_back(make_pair(x - 1, y));
+                worklist.push_back(make_pair(x, y - 1));
+
+            }
+        }
+    }
+    void solve(vector<vector<char>> &board) {
+        int n = (int) board.size();
+        if (n <= 2) return;
+        int m = (int) board[0].size();
+        if (m <= 2) return;
+        
+        for (int i = 0; i < m - 1; ++i) {
+            mark(board, 0, i);
+        }
+        for (int i = 0; i < n - 1; ++i) {
+            mark(board, i, m - 1);
+        }
+        for (int i = m - 1; i > 0; --i) {
+            mark(board, n - 1, i);
+        }
+        for (int i = n - 1; i > 0; --i) {
+            mark(board, i, 0);
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == 'Q') {
+                    board[i][j] = 'O';
                 }
             }
         }
