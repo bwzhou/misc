c77ef1a0bbcd7f8db50a49929c7bc4c64175f9ac
bwzhou <bwzhou@hotmail.com>
2014-03-22 11:28:49 -0400

    Update 128.cpp

--- leetcode/128.cpp
+++ leetcode/128.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*
     int longestConsecutive(vector<int> &num) {
         set<int> slots;
         for (const auto &each : num) {
@@ -11,16 +12,33 @@ public:
         for (set<int>::iterator it = slots.begin(), ie = slots.end(); it != ie; ++it) {
             if (it == slots.begin()) {
                 len = 1;
+                maxLen = max(maxLen, len);
             } else {
                 if (*it == prev + 1) {
                     ++len;
+                    maxLen = max(maxLen, len);
                 } else {
                     len = 1;
                 }
             }
-            maxLen = max(maxLen, len);
             prev = *it;
         }
         return maxLen;
+    }
+*/
+    int longestConsecutive(vector<int> &num) {
+        unordered_map<int, int> count;
+        int maxConsec = 0;
+        for (auto &each : num) {
+            if (count[each]) {
+                continue;
+            }
+            count[each] = 1;
+            int left{count[each - 1]};
+            int right{count[each + 1]};
+            count[each - left] = count[each + right] = 1 + left + right; ////
+            maxConsec = max(maxConsec, 1 + left + right);
+        }
+        return maxConsec;
     }
 };
