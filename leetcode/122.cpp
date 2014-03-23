3f55a5fb967dc5637ca8b0cceb01be87ab6d073b
bwzhou <bwzhou@hotmail.com>
2014-03-23 14:14:18 -0400

    Update 122.cpp

--- leetcode/122.cpp
+++ leetcode/122.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*
     int maxProfit(vector<int> &prices) {
         int profit = 0;
         int start = 0;
@@ -14,6 +15,24 @@ public:
             }
             profit += prices[end] - prices[start];
             ++end;
+        }
+        return profit;
+    }
+*/
+    int maxProfit(vector<int> &prices) {
+        int profit = 0;
+        int buy = 0;
+        int sell = 0;
+        for (int i = 1; i < prices.size(); ++i) {
+            if (prices[i] < prices[i - 1]) {
+                profit += prices[sell] - prices[buy];
+                buy = sell = i;
+            } else if (prices[i] >= prices[i - 1]) {
+                sell = i;
+                if (i == prices.size() - 1) {
+                    profit += prices[sell] - prices[buy];
+                }
+            }
         }
         return profit;
     }
