c18bbd6cc9dde91741bfc18b43e2a4bb815436ec
bwzhou <bwzhou@hotmail.com>
2014-03-23 15:18:48 -0400

    Update 119.cpp

--- leetcode/119.cpp
+++ leetcode/119.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*
     vector<int> getRow(int rowIndex) {
         vector<vector<int>> row(2, vector<int>(rowIndex + 1, 0));
         row[0][0] = 1;
@@ -10,5 +11,21 @@ public:
             }
         }
         return row[rowIndex % 2];
+    }
+*/
+    vector<int> getRow(int k) {
+        vector<int> p(k + 1, 0);
+        int tmp1 = 0;
+        int tmp2 = 0;
+        p[0] = 1;
+        for (int i = 1; i <= k; ++i) {
+            //tmp1 = 0; // this can be skipped since the last element of each row is initialized to 0
+            for (int j = 0; j <= i; ++j) {
+                tmp2 = tmp1;
+                tmp1 = p[j];
+                p[j] += tmp2;
+            }
+        }
+        return p;
     }
 };
