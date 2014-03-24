bd62e5300ce8d4627de3ffe834231ba635a4b0bc
bwzhou <bwzhou@hotmail.com>
2014-03-23 21:50:29 -0400

    Update 115.cpp

--- leetcode/115.cpp
+++ leetcode/115.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*
     int numDistinct(string S, string T) {
         if (S.empty()) {
             return 0;
@@ -22,5 +23,47 @@ public:
             result += each;
         }
         return result;
+    }
+*/
+/*
+    int numDistinct(string S, string T) {
+        int m = S.size();
+        int n = T.size();
+        int tmp1, tmp2;
+        // count[i, j] = count[i - 1, j] + S[i] == T[j] ? count[i - 1, j - 1] : 0
+        // count[i, 0] = count[i - 1, 0] + S[i] == T[j] ? 1 : 0
+        vector<int> count(m, 0); // by S
+        for (int j = 0; j < n; ++j) {
+            tmp1 = (j == 0 ? 1 : 0);
+            for (int i = 0; i < m; ++i) {
+                tmp2 = tmp1;
+                tmp1 = (j == 0 ? 1 : count[i]);
+                count[i] = (i == 0 ? 0 : count[i - 1]);
+                if (S[i] == T[j]) {
+                    count[i] += tmp2;
+                }
+            }
+        }
+        return m == 0 ? 0 : count[m - 1];
+    }
+*/
+    int numDistinct(string S, string T) {
+        int m = S.size();
+        int n = T.size();
+        int tmp1, tmp2;
+        // count[i, j] = count[i - 1, j] + S[i] == T[j] ? count[i - 1, j - 1] : 0
+        // count[i, 0] = count[i - 1, 0] + S[i] == T[j] ? 1 : 0
+        vector<int> count(n, 0); // by T
+        for (int i = 0; i < m; ++i) {
+            tmp1 = 1; // increment count[0] by 1 if S[i] matches T[0]
+            for (int j = 0; j <= min(n - 1, i); ++j) { // count[j] == 0 when j > i
+                tmp2 = tmp1;
+                tmp1 = count[j];
+                if (S[i] == T[j]) {
+                    count[j] += tmp2;
+                }
+            }
+        }
+        return count[n - 1];
     }
 };
