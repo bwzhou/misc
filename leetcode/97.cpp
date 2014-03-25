de6f72a3434ee531025f0bd2659495e0ed16b72c
bwzhou <bwzhou@hotmail.com>
2014-03-25 11:45:34 -0400

    Update 97.cpp

--- leetcode/97.cpp
+++ leetcode/97.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*
     bool isInterleave(string s1, string s2, string s3) {
         if (s1.size() + s2.size() != s3.size()) {
             return false;
@@ -13,6 +14,21 @@ public:
                 last1 = result[j]; // old result[j]
                 result[j] = (j > 0 && last2 && s1[j - 1] == s3[i - 1]) ||
                             (j < i && last1 && s2[i - j - 1] == s3[i - 1]);
+            }
+        }
+        return result[s1.size()];
+    }
+*/
+    bool isInterleave(string s1, string s2, string s3) {
+        if (s1.size() + s2.size() != s3.size()) {
+            return false;
+        }
+        vector<bool> result(s1.size(), false);
+        for (int i = 0; i <= s2.size(); ++i) {
+            for (int j = 0; j <= s1.size(); ++j) {
+                result[j] = (i > 0 && result[j] && s2[i - 1] == s3[i + j - 1]) ||
+                            (j > 0 && result[j - 1] && s1[j - 1] == s3[i + j - 1]) ||
+                            (i == 0 && j == 0);
             }
         }
         return result[s1.size()];
